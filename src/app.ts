e17e62c46f2f835fb9d75838bcbf214817873ae2
racheli546 <546rach@gmail.com>
2026-01-14 12:49:59 +0200

    chore: add error middleware to app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,17 @@
 import express from 'express';
+import { errorMiddleware } from './middlewares/error.middleware';
 
 const app = express();
 
 // middleware לקריאת JSON
 app.use(express.json());
 
+
 // בדיקת חיים – זמני
 app.get('/health', (req, res) => {
   res.send('API is running');
 });
+
+app.use(errorMiddleware);
 
 export default app;
