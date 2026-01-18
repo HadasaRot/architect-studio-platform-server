28975f522983e18df0f2f23ac061306d32ef1835
racheli546 <546rach@gmail.com>
2026-01-18 22:29:02 +0200

    feat: add clients service skeleton

---
+++ src/services/clients.service.ts
@@ -0,0 +1,7 @@
+export const createClient = async (displayName: string) => {
+    throw new Error('Not implemented');
+};
+
+export const getClientById = async (id: string) => {
+    throw new Error('Not implemented');
+}
