8a15c2b9d45087b7aedc789ca2a19ff779a4d79c
racheli546 <546rach@gmail.com>
2026-01-25 20:59:17 +0200

    feat: add base function to controller

---
+++ src/controllers/users.controller.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from 'express';
+
+export const createUser = async (req: Request, res: Response) => {
+    // Implementation for creating a user
+    return res.status(501).json({ message: 'Not implemented' });
+};
+
+export const getUserById = async (req: Request, res: Response) => {
+    // Implementation for retrieving a user by ID
+    return res.status(501).json({ message: 'Not implemented' });
+
+};
