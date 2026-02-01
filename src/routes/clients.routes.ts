c09bbdd4d807afd2837d49f63e04012540a51623
racheli546 <546rach@gmail.com>
2026-02-01 15:39:25 +0200

    feat: add route to list projects by client

--- src/routes/clients.routes.ts
+++ src/routes/clients.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { createClient, getClientById } from "../controllers/clients.controller";
+import { listClientProjects, } from '../controllers/client-projects.controller';
 
 const router = Router();
 
 router.post("/", createClient);
 router.get("/:id", getClientById);
+router.get("/:clientId/projects", listClientProjects);
 
 export default router;
